bcb08a8c79caa0fde5968133d1d872130bd80e1b
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-10-18 17:18:17 -0500

    Updated required version of Shapeshifter to avoid the need to manually like sodium .a

--- PacketTunnelExampleDependencies/Package.swift
+++ PacketTunnelExampleDependencies/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PacketTunnelExampleDependencies",
     dependencies: [
         .package(url:"https://github.com/OperatorFoundation/Transport.git", from: "0.0.12"),
-        .package(url: "https://github.com/OperatorFoundation/Shapeshifter-Swift-Transports.git", from: "0.1.0"),
+        .package(url: "https://github.com/OperatorFoundation/Shapeshifter-Swift-Transports.git", from: "0.2.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.1")
         ],
     targets: [
